3ac88694f1343a0ceaca8cd5c03f9bfe39979e12
Vivek <vivek0094.cse19@chitkara.edu.in>
2021-05-23 17:09:57 +0530

    added image bootstrap carousels

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Carousel, Col, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Product from "./Product";
 import "./Home.css";
 
@@ -6,13 +8,44 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-
-      {/* This is the img on the background */}
-      <img
-        className="home__image"
-        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-        alt=""
-      />
+      {/* This is the img on the background  having Carousel*/}
+      <Carousel className="home__image" nextLabel={""} prevLabel={""}>
+        <Carousel.Item>
+          <img
+            className=""
+            src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className=""
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img18/Fresh/GW/May21/fresh-pc_021x._CB670987717_.jpg"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className=""
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Auto/PSS/Personal-Safety_1500x600._CB668022827_.jpg"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className=""
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2021/X-site/Multititle/May/M17/non-reg/1500x600_Hero-Tall_JPN._CB668432235_.jpg"
+            alt="Forth slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className=""
+            src=" https://images-eu.ssl-images-amazon.com/images/G/31/img21/HPC/GW/Grocery_1500x600._CB669573043_.jpg"
+            alt="Fifth slide"
+          />
+        </Carousel.Item>
+      </Carousel>
 
       {/* Product component -> id, title, price, rating, image is what 
         we are passing as parameters */}
@@ -34,7 +67,61 @@ function Home() {
           image="https://www.amazon.in/images/I/41-G7AIOZyL.jpg"
         />
       </div>
-
+       <Row className="carousel-row">
+        <Carousel  nextLabel={""} prevLabel={""}>
+          <Carousel.Item>
+            <Row>
+              <Col>
+                <img
+                  className="bottom-carousel"
+                  src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/GW/Covid/shovler/1x/Shoveler_covid_donation_1x._CB671053910_.jpg"
+                  alt="First slide"
+                />
+              </Col>
+              <Col>
+                <img
+                  className="bottom-carousel"
+                  src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/GW/shoveller/Shoveler_covid_v2_smb_1x._CB669068181_.jpg"
+                  alt="First slide"
+                />
+              </Col>
+              <Col>
+                <img
+                  className="bottom-carousel"
+                  src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/GW/milaap/Shoveler_covid_homecare_1x._CB669529040_.jpg"
+                  alt="First slide"
+                />
+              </Col>
+            </Row>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Row>
+              <Col>
+                <img
+                  className="bottom-carousel"
+                  src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/GW/Covid/shovler/1x/Shoveler_covid_vaccine_1x._CB671053910_.jpg"
+                  alt="First slide"
+                />
+              </Col>
+              <Col>
+                <img
+                  className="bottom-carousel"
+                  src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/GW/milaap/Shoveler_covid_Blog_1x._CB669735929_.jpg"
+                  alt="First slide"
+                />
+              </Col>
+              <Col>
+                <img
+                  className="bottom-carousel"
+                  src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/GW/Covid/shovler/1x/Shoveler_covid_delivery__return_1x._CB671053910_.jpg"
+                  alt="First slide"
+                />
+              </Col>
+            </Row>
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+     
       <div className="home__row">
         <Product
           id="3"
@@ -47,7 +134,7 @@ function Home() {
         <Product
           id="4"
           title="How to Win Friends and Influence People Kindle Edition"
-          price={15.00}
+          price={15.0}
           rating={4}
           image="https://www.amazon.in/images/I/51bwFsBc0TL.jpg"
         />
@@ -65,7 +152,7 @@ function Home() {
         <Product
           id="6"
           title="Ikigai: The Japanese secret to a long and happy life Kindle Edition"
-          price={178.60}
+          price={178.6}
           rating={4}
           image="https://www.amazon.in/images/I/51T8OXMiB5L.jpg"
         />

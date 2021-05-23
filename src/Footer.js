436d6be95dd7a6ec72837f3dd970acf68d03264e
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2021-05-23 23:09:27 +0530

    working on footer

---
+++ src/Footer.js
@@ -0,0 +1,263 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      {/* Footer */}
+      <footer class="text-center text-lg-start bg-light text-muted">
+        {/* Section: Social media */}
+
+        {/* Section: Links */}
+        <section class="" style={{ backgroundColor: "rgb(34, 48, 62)" }}>
+          <div class="container text-center text-md-start mt-5">
+            <br />
+            {/* Grid row */}
+            <div class="row mt-3">
+              {/* Grid column */}
+              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                {/* Content */}
+                <span className="fw-bold mb-4 text-white">Get to Know Us</span>
+                <br />
+                <span className="text-white footer__span">About Us</span>
+                <br />
+                <span className="text-white footer__span">Careers</span>
+                <br />
+                <span className="text-white footer__span">Press Releases</span>
+                <br />
+                <span className="text-white footer__span">Amazon Cares</span>
+                <br />
+                <span className="text-white footer__span">Gift a Smile</span>
+              </div>
+              {/* Grid column */}
+
+              {/* Grid column */}
+              <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                {/* Links */}
+                <span class="fw-bold mb-4 text-white">Connect with Us</span>
+                <br />
+                <span className="text-white footer__span">Facebook</span>
+                <br />
+                <span className="text-white footer__span">Twitter</span>
+                <br />
+                <span className="text-white footer__span">Instagram</span>
+              </div>
+              {/* Grid column */}
+
+              {/* Grid column */}
+              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                {/* Links */}
+                <span class="fw-bold mb-4 text-white">Make Money with Us</span>
+                <br />
+                <span className="text-white footer__span">Sell on Amazon</span>
+                <br />
+                <span className="text-white footer__span">
+                  Sell under Amazon Accelerator
+                </span>
+                <br />
+                <span className="text-white footer__span">
+                  Amazon Global Selling
+                </span>
+                <br />
+                <span className="text-white footer__span">
+                  Become an Affiliate
+                </span>
+                <br />
+                <span className="text-white footer__span">
+                  Fulfilment by Amazon
+                </span>
+                <br />
+                <span className="text-white footer__span">
+                  Advertise Your Products
+                </span>
+                <br />
+                <span className="text-white footer__span">
+                  Amazon Pay on Merchants
+                </span>
+              </div>
+              {/* Grid column */}
+
+              {/* Grid column */}
+              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                {/* Links */}
+                <span class="fw-bold mb-4 text-white">Let Us Help You</span>
+                <br />
+                <span className="text-white footer__span">
+                  COVID-19 and Amazon
+                </span>
+                <br />
+                <span className="text-white footer__span">Your Account</span>
+                <br />
+                <span className="text-white footer__span">Returns Centre</span>
+                <br />
+                <span className="text-white footer__span">
+                  100% Purchase Protection
+                </span>
+                <br />
+                <span className="text-white footer__span">
+                  Amazon App Download
+                </span>
+                <br />
+                <span className="text-white footer__span">
+                  Amazon Assistant Download
+                </span>
+                <br />
+                <span className="text-white footer__span">Help</span>
+              </div>
+              {/* Grid column */}
+            </div>
+            {/* Grid row */}
+          </div>
+        </section>
+        {/* Section: Links  */}
+
+        <section style={{ backgroundColor: "rgb(19, 26, 33)" }}>
+          <br />
+          <div
+            className="row justify-content-center"
+            style={{ backgroundColor: "rgb(19, 26, 33)" }}
+          >
+            <div className="col text-center">
+              <ul className="footer__ul">
+                <li className="text-white footer__span__sm">AbeBooks</li>
+                <li className="footer__text footer__span__sm">Books, art</li>
+                <li className="footer__text footer__span__sm">
+                  &amp; collectibles
+                </li>
+              </ul>
+            </div>
+            <div className="col text-center">
+              <ul className="footer__ul">
+                <li className="text-white footer__span__sm">
+                  Amazon Web Services
+                </li>
+                <li className="footer__text footer__span__sm">
+                  Scalable Cloud
+                </li>
+                <li className="footer__text footer__span__sm">
+                  Computing Services
+                </li>
+              </ul>
+            </div>
+            <div className="col text-center">
+              <ul className="footer__ul">
+                <li className="text-white footer__span__sm">Audible</li>
+                <li className="footer__text footer__span__sm">Download</li>
+                <li className="footer__text footer__span__sm">Audio Books</li>
+              </ul>
+            </div>
+            <div className="col text-center">
+              <ul className="footer__ul">
+                <li className="text-white footer__span__sm">DPReview</li>
+                <li className="footer__text footer__span__sm">Digital</li>
+                <li className="footer__text footer__span__sm">Photography</li>
+              </ul>
+            </div>
+            <div className="col text-center">
+              <ul className="footer__ul">
+                <li className="text-white footer__span__sm">IMDb</li>
+                <li className="footer__text footer__span__sm">Movies, TV</li>
+                <li className="footer__text footer__span__sm">
+                  &amp; Celebrities
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="row" style={{ backgroundColor: "rgb(19, 26, 33)" }}>
+            <br />
+          </div>
+
+          <div
+            className="row justify-content-center"
+            style={{ backgroundColor: "rgb(19, 26, 33)" }}
+          >
+            <div className="col text-center">
+              <ul className="footer__ul">
+                <li className="text-white footer__span__sm">Shopbop</li>
+                <li className="footer__text footer__span__sm">Designer</li>
+                <li className="footer__text footer__span__sm">
+                  Fashion Brands
+                </li>
+              </ul>
+            </div>
+            <div className="col text-center">
+              <ul className="footer__ul">
+                <li className="text-white footer__span__sm">Amazon Business</li>
+                <li className="footer__text footer__span__sm">
+                  Everything For
+                </li>
+                <li className="footer__text footer__span__sm">Your Business</li>
+              </ul>
+            </div>
+            <div className="col text-center">
+              <ul className="footer__ul">
+                <li className="text-white footer__span__sm">Prime Now</li>
+                <li className="footer__text footer__span__sm">
+                  2-Hour Delivery
+                </li>
+                <li className="footer__text footer__span__sm">
+                  on Everyday Items
+                </li>
+              </ul>
+            </div>
+            <div className="col text-center">
+              <ul className="footer__ul">
+                <li className="text-white footer__span__sm">
+                  Amazon Prime Music
+                </li>
+                <li className="footer__text footer__span__sm">
+                  70 million songs, ad-free
+                </li>
+                <li className="footer__text footer__span__sm">
+                  Over 9 million podcast episodes
+                </li>
+              </ul>
+            </div>
+            <div className="col"></div>
+          </div>
+        </section>
+
+        {/* Copyright */}
+        <div
+          className="text-center"
+          style={{ backgroundColor: "rgb(19, 26, 33)" }}
+        >
+          <br />
+          <ul className="list-inline">
+            <li className="list-inline-item text-white footer__span__sm">
+              Conditions of Use &amp; Sale
+            </li>
+            <li className="list-inline-item text-white footer__span__sm">
+              Privacy Notice
+            </li>
+            <li className="list-inline-item text-white footer__span__sm">
+              Interest-Based Ads
+            </li>
+            <li className="list-inline-item text-white footer__span__sm">
+              © 1996-2021, Amazon.com, Inc. or its affiliates
+            </li>
+          </ul>
+          <br />
+        </div>
+        {/* Copyright */}
+
+        {/* Copyright */}
+        {/* <div
+          class="text-center p-4"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+        >
+          © 2021 Copyright:
+          <a class="text-reset fw-bold" href="https://mdbootstrap.com/">
+            MDBootstrap.com
+          </a>
+        </div> */}
+        {/* Copyright */}
+      </footer>
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default Footer;
